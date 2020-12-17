bef921c9c157a256e336be5fc10cc8b7853dc94a
LapiEmo <lapigerard@yahoo.fr>
2020-12-17 21:57:18 +0100

    Modification de l'énum Pays

--- backend/src/main/java/com/spiral/express/domain/enumeration/Pays.java
+++ backend/src/main/java/com/spiral/express/domain/enumeration/Pays.java
@@ -4,5 +4,5 @@ package com.spiral.express.domain.enumeration;
  * The Pays enumeration.
  */
 public enum Pays {
-    CAMEROUN, RWANDA, CONGO_BRAZZAVILLE, BURKINA_FASO
+    CAMEROUN, RWANDA, CONGO_BRAZZAVILLE, BURKINA_FASO, BELGIQUE, FRANCE, ALLEMAGNE, LUXEMBOURG
 }
