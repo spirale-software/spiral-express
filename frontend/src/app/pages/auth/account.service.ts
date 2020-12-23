fbbd4d554277f298bba3194d53cd0c070c5746f4
LapiEmo <lapigerard@yahoo.fr>
2020-12-24 00:09:04 +0100

    Account service

--- frontend/src/app/pages/auth/account.service.ts
+++ frontend/src/app/pages/auth/account.service.ts
@@ -1,10 +1,47 @@
 import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({providedIn: "root"})
 export class AccountService {
+    account: any;
+
+    constructor(private http: HttpClient) {
+    }
 
     isAuthenticated(): boolean {
         const token = sessionStorage.getItem('authenticationToken') || localStorage.getItem('authenticationToken');
         return token ? true : false;
+    }
+
+    getCurrentAccount(): any {
+        if (this.isAccountExists()) {
+            return this.getAccount();
+        } else {
+            this.http.get('api/expess/account')
+                .subscribe(
+                    res => {
+                        this.authenticate(res)
+                    },
+                    error => {
+                    }
+                );
+        }
+    }
+
+    authenticate(account) {
+        this.account = account;
+        localStorage.setItem('account', JSON.stringify(account));
+    }
+
+    getAccount(): any {
+        if (this.account) {
+            return this.account;
+        } else {
+            return JSON.parse(localStorage.getItem('account'));
+        }
+    }
+
+    isAccountExists(): boolean {
+        return this.account || localStorage.getItem('account');
     }
 }
