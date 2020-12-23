106e3f8dc7ddd5bd7edc623576e2edb99704048c
LapiEmo <lapigerard@yahoo.fr>
2020-12-24 00:17:18 +0100

    Recupérer et afficher account dans la top bar

--- frontend/src/app/app.topbar.component.ts
+++ frontend/src/app/app.topbar.component.ts
@@ -1,12 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AppComponent} from './app.component';
+import {AccountService} from "./pages/auth/account.service";
+import {Account} from "./pages/shared/model/account";
 
 @Component({
     selector: 'app-topbar',
     templateUrl: './app.topbar.component.html'
 })
-export class AppTopBarComponent {
+export class AppTopBarComponent implements OnInit {
+    account: Account;
 
-    constructor(public app: AppComponent) {}
+    constructor(public app: AppComponent, private accountService: AccountService) {
+        this.account = {} as Account;
+    }
+
+    ngOnInit(): void {
+      //  this.account = this.accountService.getCurrentAccount();
+    }
 
 }
---
+++ frontend/src/app/pages/shared/model/account.ts
@@ -0,0 +1,6 @@
+
+export interface Account {
+    nom ?: string;
+    prenom ?: string;
+    entreprise ?: string;
+}
